4c1cdffc51f70f375c689565aecd5afc12883d95
TobiasKlanert <tobias.klanert@t-online.de>
2024-10-31 22:25:44 +0100

    added fetch contacts function

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const contactsURL = "https://join-ce104-default-rtdb.europe-west1.firebasedatabase.app/contacts";
+let contacts = {};
+
 function changeImage(button, imageUrl) {
     button.querySelector("img").src = imageUrl;
+}
+
+async function getContacts(){
+    try {
+        let response = await fetch(contactsURL + ".json");
+        let responseToJSON = await response.json();
+        console.log(responseToJSON);
+        
+    } catch (error) {
+        console.log("Error");
+        
+    }
 }

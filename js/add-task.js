58bac88ee62541f1b0da85bb4550bdeb266d79e6
cp13-max <max.brik@web.de>
2024-12-15 20:01:52 +0100

    jsdoc update

--- js/add-task.js
+++ js/add-task.js
@@ -8,6 +8,14 @@ let isTitleSet,
   isCategorySet,
   isDateSet = false;
 
+/**
+ * this function loads html templates, renders initial-icons, sets the mediums priority-button
+ * and sets submit to true, so an formsubmission only happens on this page because the createTask
+ * function is used elsewhere
+ * 
+ * @param {*} elementId is the add task in the menu bar
+ * @param {*} elementType gives a class, so the background is highlighted(darker)
+ */
 async function initAddTask(elementId, elementType) {
   await loadTemplates(elementId, elementType);
   await loadAddTask();
@@ -16,29 +24,9 @@ async function initAddTask(elementId, elementType) {
   submit = true;
 }
 
-// function enableSubmit() {
-//   let categorySet = false;
-
-//   let titleValue = document.getElementById('title').value
-//   let dateValue = document.getElementById('due-date').value
-//   let categoryValue = document.getElementById('category-default-option').innerText
-//   if (categoryValue == 'Technical Task' || categoryValue == 'User Story') {
-//     categorySet = true
-//   }
-//   if (titleValue && dateValue && categorySet) {
-
-//     document.getElementById('create-task-button').removeAttribute('disabled', 'true')
-//     console.log(true);
-//   } else {
-//     console.log(false);
-//     document.getElementById('create-task-button').setAttribute('disabled', 'true')
-//   }
-// }
-
-async function initAddTaskTemplate() {
-  assignContacts();
-}
-
+/**
+ * this function loads the add task html template into the main of this page
+ */
 async function loadAddTask() {
   await loadTemplate(
     "main-add-task",
@@ -46,31 +34,49 @@ async function loadAddTask() {
   );
 }
 
+/**
+ * this function cheks if all required inputs exists,
+ * afterwards all inputs are added to the task array and tasks can be rendered.
+ * if a submission happens, page goes to board and render tasks
+ */
 function createTask() {
-  let category = document.getElementById("category-default-option").innerHTML;
+  let categoryText = document.getElementById("category-default-option").innerHTML;
+  let category;
+  if(categoryText == "Technical Task" || categoryText == "User Story"){
+    category = categoryText
+  }
   let dueDate = document.getElementById("due-date").value;
   let title = document.getElementById("title").value;
 
-  let checkCat;
+  checkRequiredInput(title, "required-title")
+  checkRequiredInput(dueDate, "required-due-date")
+  checkRequiredInput(category, "required-category")
+  if (category && dueDate && title) {
+  addTaskAndSubmit()
+  }
+}
+
+/**
+ * this function checks if a required input exist,
+ * if not a reminder text is shown below input-element
+ * 
+ * @param {*} stat is the value of the specific input
+ * @param {*} eleId id of the specific reminder text
+ */
+function checkRequiredInput(stat, eleId) {
+  if(!stat){
+  toggleDisplayNone(eleId)
+  }
+}
+
+/**
+ * this function pushes a new created task to the task array.
+ * task is localy stored, so it can be rendered if a submission(pagereload) happens.
+ * if a submission happens it is delayed, so an animation can play,
+ * if not it means we are on the board page, so tasks are rendered and dialog window is closed afterwards
+ */
+function addTaskAndSubmit() {
   
-
-  if (category != "Technical Task" && category != "User Story") {
-    toggleDisplayNone("required-category");
-    console.log("Keine Kategorie ausgewählt");
-    checkCat = false;
-  } else {
-    checkCat = true;
-  }
-
-  if (!title) {
-    toggleDisplayNone("required-title");
-    console.log("Kein Titel");
-  }
-  if (!dueDate) {
-    toggleDisplayNone("required-due-date");
-    console.log("Kein Due Date ausgewählt");
-  }
-  if (checkCat && dueDate && title) {
     pushToTasks();
     saveToLocalStorage("tasks", tasks);
 
@@ -85,8 +91,12 @@ function createTask() {
       closeDialog('boardAddTaskDialog', 'overlay-placeholder');
     }
   }
-}
 
+/**
+ * this function gets all values from the inputs, creates a new task 
+ * and set its attributes equal to the values.
+ * new task is pushed to task array and ist stored localy
+ */
 async function pushToTasks() {
   let task = {};
   let category = document.getElementById("category-default-option").innerHTML;
@@ -98,14 +108,42 @@ async function pushToTasks() {
   let subtaskElements = document.getElementById("subtasks-container").children;
 
   for (let index = 0; index < subtaskElements.length; index++) {
-    let subtask = document.getElementById(
-      "subtask-option-text-" + index
-    ).innerText;
-    let subtaskObj = { done: false };
-    subtaskObj.title = subtask;
-    console.log(subtaskObj);
-    subtasks.push(subtaskObj);
+    getSubtask(index)
   }
+
+  setObjAttributes(task,category,dueDate,title,description,assignedTo,prio,subtaskElements)
+  tasks.push(task);
+  saveToLocalStorage("tasks", tasks);
+}
+
+/**
+ * this function gets all subtasks as objects and stores them in the subtask array
+ * 
+ * @param {*} index is the iteration/number of the current subtask
+ */
+function getSubtask(index) {
+  let subtask = document.getElementById(
+    "subtask-option-text-" + index
+  ).innerText;
+  let subtaskObj = { done: false };
+  subtaskObj.title = subtask;
+  console.log(subtaskObj);
+  subtasks.push(subtaskObj);
+  }
+
+
+/**
+ * this function sets the attributes of the new task 
+ * 
+ * @param {*} task this is the new task as an empty object
+ * @param {*} category this is the category of the task, technical task or user story
+ * @param {*} dueDate this is the date to which the task is to be finished
+ * @param {*} title this is the title of the task
+ * @param {*} description this is a description of what to do
+ * @param {*} assignedTo these are the worker assigned to this task
+ * @param {*} prio this is the priority of the task
+ */
+function setObjAttributes(task,category,dueDate,title,description,assignedTo,prio) {
   task.category = category;
   task.dueDate = dueDate;
   task.title = title;
@@ -114,11 +152,6 @@ async function pushToTasks() {
   task.prio = prio;
   task.subtasks = subtasks;
   task.status = currentStatus;
-  tasks.push(task);
-  /* let taskJSON = JSON.stringify(task);
-  localStorage.setItem("task", taskJSON); */
-  saveToLocalStorage("tasks", tasks);
-  console.log(tasks);
 }
 
 function getNewTaskPrio() {
@@ -128,6 +161,10 @@ function getNewTaskPrio() {
   }
 }
 
+/**
+ * this function allowes to write subtasks in an div element that is contentEditable
+ * and it removes an onclick function, so another onclick doesnt toggle this
+ */
 function writeSubtask() {
   let parent = document.getElementById("subtask-default-option-container");
   parent.removeAttribute("onclick", "writeSubtask()");
@@ -135,6 +172,10 @@ function writeSubtask() {
   changeSubtask();
 }
 
+/**
+ * this function helps to write in an contentEditable div by making it empty, focusing it and changing the color.
+ * it also renders images that enables to either submit the written subtask or to set the default state
+ */
 function changeSubtask() {
   let subtask = document.getElementById("subtask-default-option");
   let subtaskImg = document.getElementById("subtask-img");
@@ -152,6 +193,11 @@ function changeSubtask() {
   }
 }
 
+/**
+ * this function sets the default state of the subtask contentEditable div
+ * 
+ * @param {*} event this is an onclick event
+ */
 function closeWriteSubtask(event) {
   let parent = document.getElementById("subtask-default-option-container");
   parent.setAttribute("onclick", "writeSubtask()");
@@ -160,6 +206,11 @@ function closeWriteSubtask(event) {
   changeSubtask();
 }
 
+/**
+ * this function allowes to submit a subtask by pressing the enter button
+ * 
+ * @param {*} event this is a keypress event
+ */
 function submitIfEnter(event) {
   if (event.key === "Enter") {
     submitSubtask();
@@ -167,6 +218,12 @@ function submitIfEnter(event) {
   }
 }
 
+/**
+ * this function renders two images with onclick events,
+ * one submits the subtask, the other sets the default state of the contentEditable subtask div
+ * 
+ * @returns 
+ */
 function renderSubtaskImg() {
   return `
         <div class="dropdown-arrow">
@@ -187,6 +244,11 @@ function renderSubtaskImg() {
     `;
 }
 
+/**
+ * this function renders the submitted subtasks in a different div
+ * it gives the subtasks id's and it empties and focuses back on the contentEditable div, 
+ * so another subtask can be written conveniently
+ */
 function submitSubtask() {
   let textField = document.getElementById("subtask-default-option");
   let value = textField.innerHTML;
@@ -197,6 +259,12 @@ function submitSubtask() {
   textField.focus();
 }
 
+/**
+ * this function is the rendered html template for the subtasks 
+ * 
+ * @param {*} value this is what is written in the contentEditable subtask div
+ * @returns 
+ */
 function renderSubtask(value) {
   return `
   <ul id="subtask-option-${subtaskIdCounter}" class="subtask-options hover-enabler"> 
@@ -218,18 +286,48 @@ function renderSubtask(value) {
   `;
 }
 
+
+/**
+ * this functions deletes the associated rendered subtask option
+ * 
+ * @param {*} id this is the element id of the to deleted div
+ */
 function deleteSubtaskOption(id) {
   let listElement = document.getElementById("subtask-option-" + id);
   listElement.remove();
 }
 
+/**
+ * this function enables editing of the renderend subtask options
+ * it makes associated divs contentEditable and it focuses on them
+ * it toggles classes on hovering and focus
+ * and it sets properties for the clickable imgages inside the div(s)
+ * 
+ * @param {*} id this is the element id of the div that is to be edited
+ */
 function editSubtaskOption(id) {
   let contentDiv = document.getElementById("subtask-option-" + id);
   let subtaskOptions = document.getElementById("subtaskOptions");
   let text = document.getElementById("subtask-option-text-" + id);
   let firstImg = document.getElementById("first-subtask-img-" + id);
   let secondImg = document.getElementById("second-subtask-img-" + id);
-  text.contentEditable = "true";
+
+  focusTextEnd(text) 
+
+  contentDiv.classList.toggle("hover-enabler");
+  contentDiv.classList.toggle("blue-underline");
+  subtaskOptions.classList.toggle("subtask-options-visibility");
+  
+  setImgPropertiesOnEdit(id, firstImg, secondImg)
+}
+
+/**
+ * this function makes a div contenEditable and it focuses on the end of the text inside said div
+ * 
+ * @param {*} text this is the container in which the text lies
+ */
+function focusTextEnd(text) {
+  text.contentEditable = "true"
   text.focus();
   let textLength = text.innerText.length;
   let selection = window.getSelection();
@@ -238,19 +336,32 @@ function editSubtaskOption(id) {
   range.collapse(false);
   selection.removeAllRanges();
   selection.addRange(range);
-
-  contentDiv.classList.toggle("hover-enabler");
-  contentDiv.classList.toggle("blue-underline");
-  subtaskOptions.classList.toggle("subtask-options-visibility");
-  firstImg.src = "../assets/img/delete.svg";
-  firstImg.setAttribute("onclick", `deleteSubtaskOption(${id})`);
-
-  secondImg.src = "../assets/img/check.svg";
-  secondImg.style.filter = "invert(1)";
-  secondImg.setAttribute("onclick", `saveChange(${id}, event)`);
 }
 
+/**
+ * this function sets attributes for two images
+ * 
+ * @param {*} id this is id of the element in which the images are in
+ * @param {*} img1 this is the first image
+ * @param {*} img2 this is the secons image
+ */
+function setImgPropertiesOnEdit(id, img1, img2) {
+  img1.src = "../assets/img/delete.svg";
+  img1.setAttribute("onclick", `deleteSubtaskOption(${id})`);
+  
+  img2.src = "../assets/img/check.svg";
+  img2.style.filter = "invert(1)";
+  img2.setAttribute("onclick", `saveChange(${id}, event)`);
+}
+
+/**
+ * this function sets the default state of a div by removing contentEditable
+ * and setting default properties to two clickable images
+ * 
+ * @param {*} id id of the element that is set to default
+ */
 function saveChange(id) {
+  document.getElementById("subtask-option-text-" + id).contentEditable = "false"
   let contentDiv = document.getElementById("subtask-option-" + id);
   let subtaskOptions = document.getElementById("subtaskOptions");
   contentDiv.classList.toggle("hover-enabler");
@@ -265,6 +376,9 @@ function saveChange(id) {
   secondImg.setAttribute("onclick", `deleteSubtaskOption(${id})`);
 }
 
+/**
+ * this function 
+ */
 function animateTaskCreated() {
   let animatedElement = document.getElementById("task-added-container");
   animatedElement.classList.add("animate-task-added");
